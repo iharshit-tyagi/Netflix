5e8115debc5351ef720061823930048d5bb6c21a
Harshit Tyagi <harshitt152@gmail.com>
2023-12-02 15:22:21 +0530

    Initial Commit

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const Body = () => {
+  const approute = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={approute} />
+    </div>
+  );
+};
+
+export default Body;
